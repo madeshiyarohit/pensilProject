fd7a52216d72cdf2fa33d887074dcc458be3bf7a
Rohit Madeshiya <rohitmadeshia24@gmail.com>
2022-08-22 21:32:57 +0530

    init

---
+++ Assets/index.js
@@ -0,0 +1,12 @@
+export const images = {
+  img1: require('./c-1.png'),
+  img2: require('./c-2.png'),
+  img3: require('./c-3.png'),
+  img4: require('./c-4.png'),
+  img5: require('./c-5.png'),
+  img6: require('./c-6.png'),
+  img7: require('./c-7.png'),
+  img8: require('./c-8.png'),
+  img9: require('./c-9.png'),
+  img10: require('./c-10.png'),
+};
